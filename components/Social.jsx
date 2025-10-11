0bd08fdea9cf8cb87bc629be662a100ae02e4074
Citizshrestha <citizshresthaa@gmail.com>
2025-10-11 22:00:53 +0545

    color changes for socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 import {FaGithub, FaLinkedinIn, FaInstagramSquare, FaTwitter} from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub/>, path:"https://github.com/Citizshrestha", hoverClass: "social-github"},
-    {icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/citiz-shrestha-00805b249/", hoverClass: "social-linkedin"},
-    {icon: <FaInstagramSquare/>, path:"https://www.instagram.com/citizshresthaa/", hoverClass: "social-instagram"},
-    {icon: <FaTwitter/>, path:"https://x.com/citizShrestha", hoverClass: "social-twitter"},
+    {icon: <FaGithub/>, path:"https://github.com/Citizshrestha", styleClass: "social-github"},
+    {icon: <FaLinkedinIn/>, path:"https://www.linkedin.com/in/citiz-shrestha-00805b249/", styleClass: "social-linkedin"},
+    {icon: <FaInstagramSquare/>, path:"https://www.instagram.com/citizshresthaa/", styleClass: "social-instagram"},
+    {icon: <FaTwitter/>, path:"https://x.com/citizShrestha", styleClass: "social-twitter"},
 ]
 const Social = ({containerStyles, iconStyles}) => {
   return (
     <div className={containerStyles}>
         {socials.map((social, idx) => {
            return (
-           <Link key={idx} href={social.path} className={`${iconStyles} ${social.hoverClass}`} target="_blank" rel="noopener noreferrer">
+           <Link key={idx} href={social.path} className={`${iconStyles} ${social.styleClass}`} target="_blank" rel="noopener noreferrer">
               {social.icon}
            </Link>
         );
