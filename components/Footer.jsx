4bc5da55960cd9066c35c453543476c95bfb7d6f
Citizshrestha <citizshresthaa@gmail.com>
2025-10-11 21:46:57 +0545

    small changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-[40px] px-4 border-t border-border/80 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <footer className="w-full h-auto py-3 px-4 border-t border-border/80 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto">
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-1 xl:text-xl sm:text-sm text-foreground/80 dark:text-foreground/60">
-          <div className="flex items-center gap-1 xl:pt-10 pt-5 flex-wrap justify-center">
-            <span className="xl:pl-45">Built from scratch with <span className="text-accent">💚</span> </span>
-             <span>© Copyright 2025, CitizShrestha.</span>
-            <span> ALL Rights Reserved</span>
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-1 text-xs sm:text-sm xl:text-base text-foreground/80 dark:text-foreground/60">
+          <div className="flex flex-col sm:flex-row items-center gap-1 text-center">
+            <span>Built from scratch with <span className="text-accent">💚</span></span>
+            <span>© Copyright 2025, CitizShrestha.</span>
+            <span>ALL Rights Reserved</span>
           </div>
         </div>
       </div>
