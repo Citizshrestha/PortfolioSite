fd5caa30921c1755e3e9ebedaa503eced4923b9f
citizshrestha <citizshresthaa@gmail.com>
2026-09-03 12:39:36 +0545

    update footer copyright year to latest current year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,13 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="w-full h-auto py-3 px-4 border-t border-border/80 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto">
         <div className="flex flex-col sm:flex-row justify-center items-center gap-1 text-xs sm:text-sm xl:text-base text-foreground/80 dark:text-foreground/60">
           <div className="flex flex-col sm:flex-row items-center gap-1 text-center">
             <span>Built from scratch with <span className="text-accent">💚</span></span>
-            <span>© Copyright 2025, CitizShrestha.</span>
+            <span>© Copyright {currentYear}, CitizShrestha.</span>
             <span>ALL Rights Reserved</span>
           </div>
         </div>
