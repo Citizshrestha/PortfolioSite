184e56c153f91c3a31c0a457a2d371e3aca023fd
citizshrestha <citizshresthaa@gmail.com>
2026-09-03 15:39:54 +0545

    Fix font paths for Vercel deployment

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,42 +11,42 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 const jetbrainsMono = localFont({
   src: [
     {
-      path: '../public/assets/fonts/JetBrainsMono-Thin.woff2',
+      path: './fonts/JetBrainsMono-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-ExtraLight.woff2',
+      path: './fonts/JetBrainsMono-ExtraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-Light.woff2',
+      path: './fonts/JetBrainsMono-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-Regular.woff2',
+      path: './fonts/JetBrainsMono-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-Medium.woff2',
+      path: './fonts/JetBrainsMono-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-SemiBold.woff2',
+      path: './fonts/JetBrainsMono-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-Bold.woff2',
+      path: './fonts/JetBrainsMono-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/assets/fonts/JetBrainsMono-ExtraBold.woff2',
+      path: './fonts/JetBrainsMono-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
